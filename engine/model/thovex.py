02e161b76ed45749bc823cd9625cc5d4d63956e8
sashidhar498 <sashidhar498@gmail.com>
2023-05-09 10:50:05 +0530

    updated

--- engine/model/thovex.py
+++ engine/model/thovex.py
@@ -6,4 +6,4 @@ from engine.capulet_engine import CapuletEngine
 class Thovex(CapuletEngine):
     def needs_service(self):
         service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
-        return check(service_threshold_date)
+        return self.check(service_threshold_date)
